1fcbb6eb4872b657837bc3ba58bcb1a3186a6327
lxrdrgnbrn <65499326+lxrdrgnbrn@users.noreply.github.com>
2021-03-04 13:27:20 +0900

    Add files via upload

---
+++ Lab_1/Lab_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static string DecToOct(int dec)
+        {
+            string s = "";
+            string oct="";
+            int ost;
+            while (dec>=8)
+            {
+                ost = dec % 8;
+                s = s + Convert.ToString(ost);
+                dec = dec / 8;
+            }
+            s = s + Convert.ToString(dec);
+            for (int i=s.Length; i>0; i--)
+                oct = oct + s[i-1];
+            return oct;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Десятичное число: ");
+            int d =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Восьмиричное: {DecToOct(d)}");
+            Console.ReadKey();
+
+        }
+    }
+}
